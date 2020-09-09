abe860cc0d6f5f8f47948af54443d774f7bc3a89
JorgeManuel-AR <70909373+JorgeManuel-AR@users.noreply.github.com>
2020-09-09 11:44:51 -0500

    Add files via upload

---
+++ Equipo_3/Prescalador_Tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.09.2020 11:37:52
+// Design Name: 
+// Module Name: Prescalador_Tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Prescalador_Tb(
+
+    );
+endmodule
