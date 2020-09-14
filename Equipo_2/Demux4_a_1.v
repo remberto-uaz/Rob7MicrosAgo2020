ebf07d8800336573b30b87d486b9d80f224f405c
javieruaz <47222268+javieruaz@users.noreply.github.com>
2020-09-14 12:12:36 -0500

    Update Demux4_a_1.v

--- Equipo_2/Demux4_a_1.v
+++ Equipo_2/Demux4_a_1.v
@@ -6,16 +6,16 @@ module Demux_4_a_1
     input [n-1:0] i_Datos_2,
     input [n-1:0] i_Datos_3,
     input [1:0] i_sel, // entrada selectora del mux
-    output reg [n-1:0] o_Demux // salida
+        output reg [n-1:0] o_demux // salida
     );
     
     always @(i_Datos_0, i_Datos_1, i_Datos_2, i_Datos_3, i_sel)
     begin
         case(i_sel) 
-            2'b00: begin o_Demux <= i_Datos_0; end
-            2'b01: begin o_Demux <= i_Datos_1; end
-            2'b10: begin o_Demux <= i_Datos_2; end
-            default : begin o_Demux <= i_Datos_3; end
+            2'b00: begin o_demux <= i_Datos_0; end
+            2'b01: begin o_demux <= i_Datos_1; end
+            2'b10: begin o_demux <= i_Datos_2; end
+            default : begin o_demux <= i_Datos_3; end
         endcase
     end
 
