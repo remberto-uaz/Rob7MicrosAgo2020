21bd16748f5950f74b83ff5470bc765c7d0b8c93
jfxclz <71027267+jfxclz@users.noreply.github.com>
2020-09-17 19:13:06 -0500

    Update controlador_display_7_segmentos_tb.v

--- Equipo_1/controlador_display_7_segmentos_tb.v
+++ Equipo_1/controlador_display_7_segmentos_tb.v
@@ -1,3 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: UAZ
+// Engineer: Alfa
+// 
+// Create Date: 07.09.2020 10:37:39
+// Design Name:Controlador_display_7Segmentos_tb 
+// Module Name: Controlador_display_7Segmentos_tb
+// Project Name: Controlador_display_7Segmentos_tb
+// Target Devices: Basys 3 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module Controlador_display_7Segmentos_tb; 
     reg [3:0] i_Datos_0;
     reg [3:0] i_Datos_1;
@@ -8,7 +30,7 @@ module Controlador_display_7Segmentos_tb;
     wire [6:0] o_Segmentos;
     wire [3:0] o_Anodo_4_Bits;
     
-    controlador_display_7segmentos uut(
+    Controlador_display_7segmentos uut(
      .i_Datos_0(i_Datos_0),
      .i_Datos_1(i_Datos_1),
      .i_Datos_2(i_Datos_2),
@@ -21,18 +43,21 @@ module Controlador_display_7Segmentos_tb;
   
 initial 
 begin 
-  i_Reset= 0;
-         i_Reloj  = 0;
-        i_Datos0= 4'd1;
-        i_Datos1= 4'd2;
-        i_Datos2= 4'd3;
-        i_Datos3= 4'd4;
-        #2 
-        i_Reset = 1;    
-
-        
-  end 
-    always@(i_Reloj)
-       #1 i_Reloj  = ~i_Reloj ; 
+    i_Reset= 0;
+    i_Reloj  = 0;
+    i_Datos_0= 4'd0;
+    i_Datos_1= 4'd0;
+    i_Datos_2= 4'd0;
+    i_Datos_3= 4'd0;
+    #2 
+    i_Datos_0= 4'd1;
+    i_Datos_1= 4'd2;
+    i_Datos_2= 4'd3;
+    i_Datos_3= 4'd4;
+    i_Reset = 1;    
+     
+end 
+always
+    #1 i_Reloj  = ~i_Reloj ; 
 endmodule
 
