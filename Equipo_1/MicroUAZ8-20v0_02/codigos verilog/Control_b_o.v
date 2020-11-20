8689ffd0b430d97c089aab2a8f75e608658f14a1
jfxclz <71027267+jfxclz@users.noreply.github.com>
2020-11-20 14:47:26 -0600

    Update and rename Equipo_1/MicroUAZ8-20v0_02/Control_b_o.v to Equipo_1/MicroUAZ8-20v0_02/codigos verilog/Control_b_o.v

--- Equipo_1/MicroUAZ8-20v0_02/codigos verilog/Control_b_o.v
+++ Equipo_1/MicroUAZ8-20v0_02/codigos verilog/Control_b_o.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company:uaz 
+// Engineer:team alfa
 // 
 // Create Date: 09.10.2020 19:41:26
 // Design Name: 
@@ -65,6 +65,21 @@ module Control_b_o(
                         o_Addres_Data_Bus<=Rx;
                         RW<=1;
                     end
+                default: 
+                    begin
+                        o_DataOut_Bus<=0;
+                        o_Addres_Data_Bus<=0;
+                        RW<=0;
+                    end                
             endcase
             end
+            
+// nesecario para load RX [RY] con memorias          
+    always @(Rx,Ry,NUM,Sel_Op_OutBus) begin
+         if (Sel_Op_OutBus==2'b01)
+             begin
+                 o_DataOut_Bus<=0;
+                o_Addres_Data_Bus<=Ry;
+             end
+         end
 endmodule
